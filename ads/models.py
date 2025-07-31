ba4f2c3f4bc3321a6211f8797dbc38fb8567edee
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-31 20:38:46 +1000

    creating an ad model

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
