bc4b25667287aae3d0d29bdd58aad374902e9f07
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-31 22:22:05 +1000

    check API

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/token/", include("users.jwt_urls")),
     path("api/ads/", include("ads.urls")),
-    path("api/users/", include("users.urls")),  # пока заглушка
+    # path("api/users/", include("users.urls")),  # пока заглушка
     path("api/auth/", include("rest_framework.urls")),
 ]
