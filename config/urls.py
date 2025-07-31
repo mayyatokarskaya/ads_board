3c079fc08223a8ec58d17bec612084f6d1d2d87d
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-31 21:43:20 +1000

    create routes for users

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/token/", include("users.jwt_urls")),
+    path("api/ads/", include("ads.urls")),
+    path("api/users/", include("users.urls")),  # пока заглушка
+    path("api/auth/", include("rest_framework.urls")),
 ]
