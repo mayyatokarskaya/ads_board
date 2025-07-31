1fca2a4823ea28ec6d384c7a6ed61fec88c1c437
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-31 20:52:59 +1000

    ad model registration

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ad
 
-# Register your models here.
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price", "created_at")
+    search_fields = ("title", "description", "author__email")
+    list_filter = ("created_at", "price")
+    ordering = ("-created_at",)
