effc86eb87d3a6ac39da5a3b571c4fa783238fbf
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-08-05 22:52:28 +1000

    setting PasswordReset

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from users.views import RegisterView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
+
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
